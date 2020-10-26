8bb3a47e0c04839c44ae4fe604f4f45beb13965c
Jan-Pascal van Best <janpascal@vanbest.org>
2020-10-26 21:33:02 +0100

    Attempting to get discovery recognise this integration

--- custom_components/rfidpad/config_flow.py
+++ custom_components/rfidpad/config_flow.py
@@ -5,10 +5,17 @@ import voluptuous as vol
 _LOGGER = logging.getLogger(__name__)
 
 class RFIDPadConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    async def async_step_mqtt(self, info):
-        if info is not None:
-            pass  # TODO: process info
+    async def async_step_user(self, info):
+        _LOGGER.info("async_step_user: " + str(info))
 
+        await self.async_set_unique_id(device_unique_id)
+        self._abort_if_unique_id_configured()
+        return self.async_show_form(
+            step_id="user", data_schema=vol.Schema({vol.Required("password"): str})
+        )
+
+
+    async def async_step_mqtt(self, info):
         _LOGGER.info("async_step_mqtt: " + str(info))
 
         await self.async_set_unique_id(device_unique_id)
@@ -16,3 +23,10 @@ class RFIDPadConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="mqtt", data_schema=vol.Schema({vol.Required("password"): str})
         )
+
+
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return RFIDPadOptionsFlowHandler(config_entry)
