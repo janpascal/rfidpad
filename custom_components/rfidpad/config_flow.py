1dc48fa69b7a70bd4421ee2a2b7f2826d5e5ab10
Jan-Pascal van Best <janpascal@vanbest.org>
2020-10-28 14:54:56 +0100

    More experiments with config

--- custom_components/rfidpad/config_flow.py
+++ custom_components/rfidpad/config_flow.py
@@ -1,7 +1,8 @@
 import logging
 
 from homeassistant import config_entries
-from .const import DOMAIN
+from homeassistant.core import callback
+from .const import ( DOMAIN, PLATFORMS )
 
 import voluptuous as vol
 
@@ -33,3 +34,38 @@ class RFIDPadConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @callback
     def async_get_options_flow(config_entry):
         return RFIDPadOptionsFlowHandler(config_entry)
+
+
+class RFIDPadOptionsFlowHandler(config_entries.OptionsFlow):
+    """RFIDPad config flow options handler."""
+
+    def __init__(self, config_entry):
+        """Initialize HACS options flow."""
+        self.config_entry = config_entry
+        self.options = dict(config_entry.options)
+
+    async def async_step_init(self, user_input=None):  # pylint: disable=unused-argument
+        """Manage the options."""
+        return await self.async_step_user()
+
+    async def async_step_user(self, user_input=None):
+        """Handle a flow initialized by the user."""
+        if user_input is not None:
+            self.options.update(user_input)
+            return await self._update_options()
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(x, default=self.options.get(x, True)): bool
+                    for x in sorted(PLATFORMS)
+                }
+            ),
+        )
+
+    async def _update_options(self):
+        """Update config entry options."""
+        return self.async_create_entry(
+            title=self.config_entry.data.get(CONF_USERNAME), data=self.options
+        )
